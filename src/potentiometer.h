711934ab8f2297919ad4ccf0b015392304867857
damondli <dli48@calpoly.edu>
2022-11-04 10:17:17 -0700

    Update potentiometer.h

--- src/potentiometer.h
+++ src/potentiometer.h
@@ -16,14 +16,37 @@
 class potentiometer
 {
 protected:
+    // Pin to read voltage from
     uint8_t ADC_PIN;
+    // Range of ADC values determined by resolution (12 bit default)
+    const uint16_t ADC_RANGE = 4096;
+    // Voltage source to scale the ADC reading
+    const float VOLTAGE_SOURCE = 3.3;
+
+    // Offset to zero the potentiometer
+    float voltage_offset;
+
+    // Voltage to angle conversion
+    float VOLTAGE_TO_DEGREES = 1;
 
 public:
-    potentiometer(uint8_t pin);
+    // Setup object
+    potentiometer(uint8_t pin, float offset);
 
-    uint8_t adc;
+    // Value from ADC reading
+    uint16_t adc_value;
+    
+    // Voltage from ADC reading
+    float voltage;
 
-    float get_position(void);
+    // Get the position of the potentiometer
+    float get_voltage(void);
+
+    // Get the position of the potentiometer
+    float get_angle(void);
+
+    // Zero the potentiometer
+    void zero(void);
 };
 
 #endif // _POT_
