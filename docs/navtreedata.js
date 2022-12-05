cf35dffe7f8b37f021d06aab4d2f7e0197a5cd58
damondli <dli48@calpoly.edu>
2022-12-04 17:03:28 -0800

    Doxygen Test #2

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "ME507 Airheads Glider Project", "index.html", [
+  [ "ME507 Airheads Project", "index.html", [
     [ "Classes", "annotated.html", [
       [ "Class List", "annotated.html", "annotated_dup" ],
       [ "Class Index", "classes.html", null ],
